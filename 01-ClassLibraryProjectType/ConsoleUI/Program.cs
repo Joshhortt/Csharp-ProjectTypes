c59e724b092a703b2b00cf5ab96eaa910870528b
Joshhortt <joshhortt@yahoo.com>
2021-03-18 18:04:09 +0000

    Add new Project 01 'ClassLibraryProjectType'.

---
+++ 01-ClassLibraryProjectType/ConsoleUI/Program.cs
@@ -0,0 +1,26 @@
+﻿
+// C # Project Types - Class Library Project Type
+using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Generators generators = new Generators();
+            PersonModel person = new PersonModel
+            {
+                Prefix = "Mr.",
+                FirstName = "Josh",
+                LastName = "Hortt"
+            };
+
+            string message = generators.FarewellMessage(person.Prefix, person.LastName);
+
+            Console.WriteLine(message);
+
+            Console.ReadLine();
+        }
+    }
+}

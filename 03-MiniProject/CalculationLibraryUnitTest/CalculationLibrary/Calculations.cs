2afcd520d3bd037edb1f70be2fd54242d62dfe97
Joshhortt <joshhortt@yahoo.com>
2021-03-19 16:48:42 +0000

    Add new project 03 - 'CalculationLibraryUnit Tests'. Add class 'Calculations' with 4 math functions .

---
+++ 03-MiniProject/CalculationLibraryUnitTest/CalculationLibrary/Calculations.cs
@@ -0,0 +1,31 @@
+﻿// C # Project Types - Mini-Project - Unit Tests
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CalculationLibrary
+{
+	public class Calculations
+	{
+
+		public double Add(double a, double b)
+		{
+			throw new NotImplementedException();
+		}
+
+		public double Subtract(double a, double b)
+		{
+			throw new NotImplementedException();
+		}
+
+		public double Multiply(double a, double b)
+		{
+			throw new NotImplementedException();
+		}
+
+		public double Divide(double a, double b)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}

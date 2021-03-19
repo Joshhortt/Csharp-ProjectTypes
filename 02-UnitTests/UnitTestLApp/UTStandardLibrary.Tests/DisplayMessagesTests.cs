7eae86e7071c7009bdda839054cb40ccd0af24db
Joshhortt <joshhortt@yahoo.com>
2021-03-19 11:00:10 +0000

    Add new Project 02 'UnitTests'. Create a Standard Library & add xUnitProject for unit testing, Greetings at different hours of the day.

---
+++ 02-UnitTests/UnitTestLApp/UTStandardLibrary.Tests/DisplayMessagesTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnitTestStandardLibrary;
+using Xunit;
+
+// # 3 Acts to a good Unit Test: Arrange; Act; Asert. #
+// We use: expected value and Actual value keywords
+namespace UTStandardLibrary.Tests
+{
+	public class DisplayMessagesTests  // this is the DisplayMessagesTests
+	{
+		[Fact]
+		public void GreetingShouldReturnGoodMorningMessage()
+		{
+			// Arrange
+			DisplayMessages messages = new DisplayMessages();
+			string expected = "Good morning Josh";   // 'expected' keyword
+
+			// Act
+			string actual = messages.Greeting("Josh", 2);  // 'actual' keyword
+
+			// Assert (this should have happened)
+			Assert.Equal(expected, actual);
+		}
+
+		[Fact]
+		public void GreetingShouldReturnGoodAfternoonMessage()
+		{
+			// Arrange
+			DisplayMessages messages = new DisplayMessages();
+			string expected = "Good afternoon Josh";   // 'expected' keyword
+
+			// Act
+			string actual = messages.Greeting("Josh", 14);  // 'actual' keyword
+
+			// Assert (this should have happened)
+			Assert.Equal(expected, actual);
+		}
+	}
+}
---
+++ 02-UnitTests/UnitTestLApp/UnitTestStandardLibrary/DisplayMessages.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+// Test Driven Development: Red, Green, Refactor
+
+namespace UnitTestStandardLibrary
+{
+	public class DisplayMessages  // this is the DisplayMessages class
+	{
+		public string Greeting(string firstName, int hourOfTheDay)
+		{
+			string output = "";
+
+			if(hourOfTheDay < 12)
+			{
+				output = $"Good morning { firstName }";
+			}
+			else if(hourOfTheDay < 18)
+			{
+				output = $"Good afternoon { firstName }";
+			}
+			else
+			{
+				output = $"Good evening { firstName }";
+			}
+			return output;
+		}
+	}
+}

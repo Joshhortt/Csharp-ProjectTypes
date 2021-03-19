31075be407beced674d7a70e0f0ea90a38b4b2db
Joshhortt <joshhortt@yahoo.com>
2021-03-19 18:57:20 +0000

    Add more tests to verify if they pass. Add in Divide function a condition to return zero if output is a infinitive number.

--- 03-MiniProject/CalculationLibraryUnitTest/CalculationLibrary.Tests/CalculationTests.cs
+++ 03-MiniProject/CalculationLibraryUnitTest/CalculationLibrary.Tests/CalculationTests.cs
@@ -11,15 +11,67 @@ namespace CalculationLibrary.Tests
 
 		[Theory]
 		[InlineData(4, 2, 6)]
+		[InlineData(0, 0, 0)]
+		[InlineData(1200, -1200, 0)]
+		[InlineData(40, -41, -1)]
 		public void AddShouldReturnExpectedValue(double a, double b, double expected)
 		{
 			// Arrange
 			Calculations calc  = new Calculations();
 
-
 			// Act
 			double actual = calc.Add(a, b);
 
+			// Assert
+			Assert.Equal(expected, actual);
+		}
+
+		[Theory]
+		[InlineData(8, 2, 6)]
+		[InlineData(0, 2, -2)]
+		[InlineData(9, 10, -1)]
+		[InlineData(99, 109, -10)]
+		public void SubtractShouldReturnExpectedValue(double a, double b, double expected)
+		{
+			// Arrange
+			Calculations calc = new Calculations();
+
+			// Act
+			double actual = calc.Subtract(a, b);
+
+			// Assert
+			Assert.Equal(expected, actual);
+		}
+
+		[Theory]
+		[InlineData(4, 2, 8)]
+		[InlineData(1, 2, 2)]
+		[InlineData(40, 0, 0)]
+		[InlineData(3, 7, 21)]
+		public void MultiplyShouldReturnExpectedValue(double a, double b, double expected)
+		{
+			// Arrange
+			Calculations calc = new Calculations();
+
+			// Act
+			double actual = calc.Multiply(a, b);
+
+			// Assert
+			Assert.Equal(expected, actual);
+		}
+
+		[Theory]
+		[InlineData(4, 2, 2)]
+		[InlineData(1, 4, 0.25)]
+		[InlineData(6, 2, 3)]
+		[InlineData(15, 0, 0)]  // Returns infinitve number. It s necessary implement output condition to return zero.
+		public void DivideShouldReturnExpectedValue(double a, double b, double expected)
+		{
+			// Arrange
+			Calculations calc = new Calculations();
+
+			// Act
+			double actual = calc.Divide(a, b);
 
 			// Assert
 			Assert.Equal(expected, actual);

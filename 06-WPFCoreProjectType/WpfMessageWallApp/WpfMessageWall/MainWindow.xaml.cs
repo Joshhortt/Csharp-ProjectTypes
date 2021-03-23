6aa89cb75b23b002d24776550abbbf66339564e2
Joshhortt <joshhortt@yahoo.com>
2021-03-23 16:08:54 +0000

    Add FC of Project 06 WPF Core project type. Simple Message Wall Application.

---
+++ 06-WPFCoreProjectType/WpfMessageWallApp/WpfMessageWall/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿// 06 - C # Project Types - WPF Core
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfMessageWall
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+        BindingList<string> messages = new BindingList<string>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            messageList.ItemsSource = messages;
+        }
+
+        private void AddMessage_Click(object sender, RoutedEventArgs e)
+        {
+            messages.Add(messageText.Text);
+            messageText.Text = "";
+        }
+    }
+}

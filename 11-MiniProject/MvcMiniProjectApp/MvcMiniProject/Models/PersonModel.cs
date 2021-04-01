d6ffa1134cda09b4d53672b2dea4428eb340cb79
Joshhortt <joshhortt@yahoo.com>
2021-04-01 19:36:17 +0100

    Add AddressModel with properties, add PersonModel with properties.

---
+++ 11-MiniProject/MvcMiniProjectApp/MvcMiniProject/Models/PersonModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcMiniProject.Models
+{
+	public class PersonModel
+	{
+		[Required]
+		[Display(Name = "First Name")]
+		public int FirstName { get; set; }
+
+		[Required]
+		[Display(Name = "Last Name")]
+		public int LastName { get; set; }
+
+		[Display(Name = "Active User")]
+		public bool IsActive { get; set; }
+	}
+}
